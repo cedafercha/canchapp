90b9939fc0ebd5e8d6afb1b91871a915dd007517
manuel zambrano <manuelzambrano@outlook.com>
2024-10-26 09:04:17 -0500

    creacion de layout para usuarios loguiados y no loguiados

--- projects/mf-host/src/app/app-routing.module.ts
+++ projects/mf-host/src/app/app-routing.module.ts
@@ -2,45 +2,61 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { 
-    path: 'board',
-    loadChildren: () => 
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://localhost:4202/remoteEntry.js',
-        exposedModule: './BoardModule'
-      })
-      .then(m => m.BoardModule)
-    
+  { path: '', redirectTo: '/home', pathMatch: 'full'},  
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ]
   },
-  { 
-    path: 'court',
-    loadChildren: () => 
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://localhost:4204/remoteEntry.js',
-        exposedModule: './CourtModule'
-      })
-      .then(m => m.CourtModule)
-    
-  },
-  { 
-    path: 'company',
-    loadChildren: () => 
-      loadRemoteModule({
-        type: 'module',
-        remoteEntry: 'http://localhost:4203/remoteEntry.js',
-        exposedModule: './CompanyModule'
-      })
-      .then(m => m.CompanyModule)
-    
-  }
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { 
+        path: 'board',
+        loadChildren: () => 
+          loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4202/remoteEntry.js',
+            exposedModule: './BoardModule'
+          })
+          .then(m => m.BoardModule)
+        
+      },
+      { 
+        path: 'court',
+        loadChildren: () => 
+          loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4204/remoteEntry.js',
+            exposedModule: './CourtModule'
+          })
+          .then(m => m.CourtModule)
+        
+      },
+      { 
+        path: 'company',
+        loadChildren: () => 
+          loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4203/remoteEntry.js',
+            exposedModule: './CompanyModule'
+          })
+          .then(m => m.CompanyModule)
+        
+      }
+    ]
+  }  
 ];
 
 @NgModule({
--- projects/mf-host/src/app/home/home.component.ts
+++ projects/mf-host/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+
+  constructor(private http: HttpClient) {
+    localStorage.setItem("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjEiLCJOYW1lIjoiTWFudWVsIiwiRW1haWwiOiJtYW51ZWx6YW1icmFub0BvdXRsb29rLmNvbSIsIlRlbmFudCI6IiIsImV4cCI6MTcyOTc1MDEwOX0.GEEIVIA8m0NS-CCJRLVIEtfIyEeRNaPULyjMD39vOJc");
+    console.log("Home OK");
+  }
+
+  async getData() {
+    var login = {
+      Email: 'manuelzambrano@outlook.com',
+      Password: '123456'
+    };
+    var result = await this.http.post('http://localhost:5197/api/Security/Login',login).subscribe(res => {
+      console.log(res);  
+    });
+    //var result = await this.http.get('http://localhost:5197/api/Security/GetAll');
+    console.log(result);
+  }
 
 }
