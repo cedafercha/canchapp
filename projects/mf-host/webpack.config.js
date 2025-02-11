9286d2968de036d852c1691ea9313c5019685585
manuel zambrano <manuelzambrano@outlook.com>
2025-02-11 11:33:57 -0500

    config deploy

--- projects/mf-host/webpack.config.js
+++ projects/mf-host/webpack.config.js
@@ -2,14 +2,22 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 
 module.exports = withModuleFederationPlugin({
 
+  // remotes: {
+  //   mfUser:       "http://localhost:4201/remoteEntry.js",
+  //   mfDashBoard:  "http://localhost:4202/remoteEntry.js",
+  //   mfCompany:    "http://localhost:4203/remoteEntry.js",
+  //   mfCatalog:    "http://localhost:4204/remoteEntry.js",
+  //   mfBooking:    "http://localhost:4205/remoteEntry.js",
+  //   mfReport:     "http://localhost:4206/remoteEntry.js",
+  // },
   remotes: {
     mfUser:       "http://localhost:4201/remoteEntry.js",
     mfDashBoard:  "http://localhost:4202/remoteEntry.js",
     mfCompany:    "http://localhost:4203/remoteEntry.js",
     mfCatalog:    "http://localhost:4204/remoteEntry.js",
-    mfBooking:    "http://localhost:4205/remoteEntry.js",
+    mfBooking:    "https://canchapp.github.io/canchapp/booking/remoteEntry.js",
     mfReport:     "http://localhost:4206/remoteEntry.js",
-  },
+  },  
 
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
